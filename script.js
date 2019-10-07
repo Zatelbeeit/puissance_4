b6080998d2cc4928abcff4419f9a6baa97812fb3
tomsirat <sirat.tom@outlook.com>
2019-10-07 16:22:37 +0200

    End of the Puissance 4

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-var boardElement = document.getElementsByTagName("body")[0];
-var distributorElement = document.getElementsByTagName("body")[0];
 var turn = -1
 
 var board = [
@@ -11,93 +9,97 @@ var board = [
     [0, 0, 0, 0, 0, 0, 0]
 ];
 
-var distributor = [
-    [0, 0, 0, 0, 0, 0, 0]
-];
+var distributor = [0, 0, 0, 0, 0, 0, 0];
+
+// -------------------- Rendu de la grille de jeu ------------------------------ //
 
 function render() {
+    var boardElement = document.body;
+    var distributorElement = document.createElement("table");
+    boardElement.appendChild(distributorElement)
+
+// ------------------- Création Board / Distributor -------------------------- //
+
+    var distributorRow = document.createElement("tr");
+    distributorElement.appendChild(distributorRow)
+    for (let l = 0; l < distributor.length; l++) {
+        var distributorCell = document.createElement("td");
+        distributorCell.className = "cell2"
+        distributorRow.appendChild(distributorCell)
+
+        // ---------- Create HTML element on board / Création counter on distributor -------------------------- //
+
+        var counter = document.createElement("div");
+        counter.classList.add("counter");
+        counter.setAttribute("id", l)
+        counter.style.backgroundColor = "blue";
+
+
+
+// -------------------- Affichage conter ------------------------------------ //
+
+        distributorCell.appendChild(counter)
+
+    // ------------------- Event play -------------------------------------- //
+
+        counter.addEventListener("click", play)
+    }
+
+// ----------------------- Boucle affichage cell --------------------------- //
 
     for (let i = 0; i < board.length; i++) {
         for (let j = 0; j < board[i].length; j++) {
-            
+
             var cellElement = document.createElement("div");
-            
+
             boardElement.appendChild(cellElement);
             cellElement.className = "cell";
             cellElement.setAttribute("id", i + "-" + j)
 
         }
-        
+
     }
-    
+
 }
 
 
-for (let l = 0; l < distributor.length; l++) {
-    for (let k = 0; k < distributor[l].length; k++) {
-        
-        var cell2Element = document.createElement("section");
-        distributorElement.appendChild(cell2Element);
-        cell2Element.className = "cell2";
-        cell2Element.setAttribute("id", k + "-" + l)
-        
-        var counter = document.createElement("div");
-        counter.classList.add("counter");
-        cell2Element.appendChild(counter)
-        counter.style.backgroundColor = "blue";
-
-        cell2Element.addEventListener("click", play)
-
-        
-        }
-    
-    }
-
+// ------------------------ Call function --------------------------------- //
 
 render();
 
 function play() {
-    var idAttribute = this.getAttribute("id");
-    var splitAttribute = idAttribute.split("-");
-    var row = splitAttribute[0];
-    var column = splitAttribute[0];
 
-    
-    currentColumnCells = document.querySelectorAll("div[id$='-"+column+"']:not(.played)");
-    if(currentColumnCells.length > 0) {
+    // --------------------------- Condition counter placement on board according to id------------ //
+
+    var column = this.id;
+
+
+    currentColumnCells = document.querySelectorAll("div[id$='-" + column + "']:not(.played)");
+
+    if (currentColumnCells.length > 0) {
         currentCell = currentColumnCells[currentColumnCells.length - 1]
         currentCell.classList.add("played")
-        // currentCell.appendChild(counter)
-        // counter.style.backgroundColor = "blue";
-        currentCell.innerHTML ="";
-        if(turn == -1){
-            turn = 1
-            counter.querySelectorAll(counter.style.backgroundColor = "red");
-            currentCell.style.backgroundColor ="blue";
 
-        }else{
+        var counterElements = document.getElementsByClassName("counter");
+
+// ------------------------ Tour de jeu -------------------------------------------- //
+
+        if (turn == -1) {
+            turn = 1
+            currentCell.style.backgroundColor = "blue";
+            for (let i = 0; i < counterElements.length; i++) {
+                counterElements[i].style.backgroundColor = "red";
+            }
+
+        } else {
             turn = -1
-            counter.querySelectorAll(counter.style.backgroundColor = "blue");
             currentCell.style.backgroundColor = "red";
+            for (let i = 0; i < counterElements.length; i++) {
+                counterElements[i].style.backgroundColor = "blue";
+            }
 
         }
 
-
     }
 
-    
-
-    
-    
-        
-    
-    
-   
-
-        // pour trouver la case vide avant use selector avec le .not.counter
-
-    
-    
-    
-    
 }
